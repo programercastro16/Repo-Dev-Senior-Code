ee193e7e1befbe33421533e5c055b375e1ba7612
programercastro16 <thomascastro2005@gmail.com>
2026-08-20 11:53:28 -0500

    Clase 1 Modulo 2: setup NestJS y primera API de productos

---
+++ src/productos/productos.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProductosService {
+  private productos = [
+    { id: 1, nombre: 'Teclado mecánico', precio: 120000, stock: 15 },
+    { id: 2, nombre: 'Mouse inalámbrico', precio: 45000, stock: 30 },
+     { id: 3, nombre: 'Pantalla', precio: 1000000, stock: 5 },
+  ];
+
+  obtenerTodos() {
+    return this.productos;
+  }
+
+  crear(nuevo: { nombre: string; precio: number; stock: number }) {
+    const producto = {
+      id: this.productos.length + 1,
+      nombre: nuevo.nombre,
+      precio: nuevo.precio,
+      stock: nuevo.stock,
+    };
+    this.productos.push(producto);
+    return producto;
+  }
+}
